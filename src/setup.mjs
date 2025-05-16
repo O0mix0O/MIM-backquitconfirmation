e4bbf9a4b7f73227e77ecffd0ed8739ca67e3374
Mix <m.gopkin1992@gmail.com>
2025-05-16 12:06:17 +0100

    housekeeping

--- src/setup.mjs
+++ src/setup.mjs
@@ -1,33 +1,32 @@
 (function () {
-    console.log("Confirm Quit Mod Loaded");
-
-    function showQuitConfirmation() {
-        Swal.fire({
-            icon: "warning",
-            title: "Are you sure you want to quit?",
-            text: "Press 'back' again to exit, or 'OK' to stay",
-            customClass: {
-                container: 'swal2-container swal2-center swal-infront swal2-backdrop-show',
-                popup: 'swal2-modal'
-            },
-        });
-    }
-
-    window.addEventListener("popstate", function (event) {
-        console.log("Back button detected. Preventing default navigation.");
-        event.preventDefault();
-        history.pushState(null, null, location.href);
-        showQuitConfirmation();
+  function showQuitConfirmation() {
+    Swal.fire({
+      icon: "warning",
+      title: "Are you sure you want to quit?",
+      text: "Press 'back' again to exit, or 'OK' to stay",
+      customClass: {
+        container:
+          "swal2-container swal2-center swal-infront swal2-backdrop-show",
+        popup: "swal2-modal",
+      },
     });
+  }
 
-    window.addEventListener("beforeunload", function (event) {
-        console.log("Window close attempt detected. Showing quit confirmation modal.");
-        event.preventDefault();
-        event.returnValue = '';
-        showQuitConfirmation();
-    });
-
+  window.addEventListener("popstate", function (event) {
+    console.log("Back button detected. Preventing default navigation.");
+    event.preventDefault();
     history.pushState(null, null, location.href);
+    showQuitConfirmation();
+  });
 
-    console.log("Confirm Quit Mod with aggressive back button prevention successfully initialized.");
+  window.addEventListener("beforeunload", function (event) {
+    console.log(
+      "Window close attempt detected. Showing quit confirmation modal."
+    );
+    event.preventDefault();
+    event.returnValue = "";
+    showQuitConfirmation();
+  });
+
+  history.pushState(null, null, location.href);
 })();
